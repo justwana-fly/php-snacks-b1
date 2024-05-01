c2043491b15d4a19bc2c550dea88f479aea93eee
justwana-fly <nicolaibarbarasa@gmail.com>
2024-05-01 17:28:16 +0200

    snack 4 fatto

--- form.php
+++ form.php
@@ -91,7 +91,7 @@ Creare un array di array. Ogni array figlio avrà come chiave una data in questo
 DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
 Stampare ogni data con i relativi post. 
 -->
-
+<!-- 
 <?php
 // Array di libri associati a ciascuna data
 $libri_per_data = [
@@ -121,6 +121,37 @@ foreach ($libri_per_data as $data => $libri) {
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html> -->
+
+
+
+
+
+<!-- 
+    Snack 4
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta 
+-->
+<?php
+
+$numbers = [];
+
+for ($i = 0; $i < 15; $i++) {
+    $numbers[] = rand(1, 100);
+    $numbers = array_unique($numbers);
+    $numbers = array_values($numbers);
+}
+print_r($numbers);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
